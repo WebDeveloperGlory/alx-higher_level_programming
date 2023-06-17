c532fe8801dff876c84f0405308a3dcfb532e1d2
WebDeveloperGlory <104975242+WebDeveloperGlory@users.noreply.github.com>
2023-06-17 01:57:28 +0100

    Add files via upload

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+    language that combines remarkable power with very clear syntax"
+str = str.split(",")[2].split()[0] + " " + str.split(",")[2].split()[1] + " " + str.split()[12] + " " + str.split()[0]
+print(str)
